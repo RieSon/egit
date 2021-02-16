cc729e560224f5b6b6ed782799d9202c0b9d8469
RieSon <8330186+Rieson1@user.noreply.gitee.com>
2021-02-16 09:35:50 +0800

    添加文件

---
+++ src/main/java/com/Demo/test/Hello.java
@@ -0,0 +1,15 @@
+package com.Demo.test;
+ /**
+ * @Description
+ * @author: Administrator  Email:ss1606531417@gmail.com
+ * @version: 1.0
+ * @date:2021年2月15日 下午1:54:43
+ * @phone: 13253531289
+ * 
+ */
+public class Hello {
+ public void test() {
+	System.err.println("asdsa");
+	
+}
+}
---
+++ src/main/java/com/Demo/test/Test.java
@@ -0,0 +1,12 @@
+package com.Demo.test;
+ /**
+ * @Description
+ * @author: Administrator  Email:ss1606531417@gmail.com
+ * @version: 1.0
+ * @date:2021年2月16日 上午9:19:01
+ * @phone: 13253531289
+ * 
+ */
+public class Test {
+
+}
